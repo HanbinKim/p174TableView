01389275dcfa5d946daec62329cb7338144dae6b
HanbinKim <hottuc@naver.com>
2014-01-06 15:44:54 +0900

    P174 TableVIew
    셀에 악세서리 버튼을 이용하는 실습입니다…

--- p174TableView/ViewController.m
+++ p174TableView/ViewController.m
@@ -8,16 +8,57 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define  CELL_ID @"CELL_ID"
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @end
 
 @implementation ViewController
+{
+    NSArray *data;
+}
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Select Row : %d",indexPath.row);
+}
+
+- (void) tableView: (UITableView *) tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Aceessory Button tapped : %d", indexPath.row);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    
+    if(0 == indexPath.row)
+    {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else if(1 == indexPath.row)
+    {
+        cell.accessoryType = UITableViewCellAccessoryDetailButton;
+    }
+    else if(2 == indexPath.row)
+    {
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data = @[@"a",@"b",@"c",@"d",@"f",@"g",@"h",@"i"];
 }
 
 - (void)didReceiveMemoryWarning
